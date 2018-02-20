9309e677ff573071b94320dd0826aae2f0d409eb
tysonparks <typarks@gmail.com>
2018-02-20 14:27:45 -0800

    Initial Commit

---
+++ AutoLayout_Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout_Storyboard
+//
+//  Created by Tyson Parks on 2/20/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout_Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout_Storyboard
+//
+//  Created by Tyson Parks on 2/20/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
